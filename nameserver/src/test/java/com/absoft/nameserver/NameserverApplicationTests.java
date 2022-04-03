76b46a3e1bcd8caa8320d990cf3869ae57fa7cf1
Abhishek Jha <abhi170599@gmail.com>
2022-04-03 19:26:35 +0530

    Add eureka nameserver

---
+++ nameserver/src/test/java/com/absoft/nameserver/NameserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absoft.nameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
